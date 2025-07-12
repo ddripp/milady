b13c1b9cae7f982a054a06323655d3e0c54c268d
ddripp <150576430+ddripp@users.noreply.github.com>
2025-07-12 01:51:33 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: "/milady",
+  base: "https://ddripp.github.io/milady/",
 });
